398e2be8a20329c8acac88712497128d859db3cf
K Fayaz Uddin <kfayaz.uddin@planview.com>
2024-08-18 12:28:49 +0530

    Single-Microservice-APIGateway

---
+++ ClientSide/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using ClientSide.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientSide.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await
+           _productService.GetProductsAsync();
+            return View(products);
+        }
+    }
+}
